74654c710ee67981ebabeb18eb70eeb03a826a5a
Newton-Nganga <nganga7newt@gmail.com>
2023-07-13 20:39:44 +0300

    ceasar_cipher script

---
+++ ceasar_cipher/script.py
@@ -0,0 +1,55 @@
+'''
+1. create variables to hold the ciphered and deciphered messages
+2. create a function(message ,steps)
+3. Loop through each char in the message
+   a. If char == letter ,
+   shift it n steps foward in the alphabet using ord() and chr() functions
+   b. If char !== letter,
+   leave it as it is
+   c. Append the shifted char to the variables
+4.Return the ciphered message string
+5.Create a function to decipher(message,steps)
+3. Loop through each char in the message
+   a. If char == letter ,
+   shift it n steps backward in the alphabet using ord() and chr() functions
+   b. If char !== letter,
+   leave it as it is
+   c. Append the shifted char to the variables
+7. Return deciphered message
+'''
+
+def caesar_cipher(message,shift):
+    ciphered_message  = ""
+    for char in message:
+        if char.isalpha():
+            if char.isupper():
+                ascii_offset = 65
+            else:
+                ascii_offset = 97
+            shifted_char = chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+            ciphered_message += shifted_char
+        else:
+            #preserve non alphabets
+            ciphered_message += char
+            
+    print(f'ciphered message : {ciphered_message}')
+    return ciphered_message
+
+def caesar_decipher(message,shift):
+    return caesar_cipher(message,-shift)
+
+
+def lets_cipher():
+    print("Welcome to ceasar cipher")
+    reply = int(input("Reply with 1 to encode and 2 to decode : "))
+    direction = "encode" if reply == 1 else "decode"
+    message = input(f'Type in the message to {direction} : ')
+    steps =  int(input("Type the shift number : "))
+    if direction == "encode":
+        caesar_cipher(message,steps)
+    elif direction == "decode":
+        caesar_decipher(message,steps)
+    else:
+        print("unsupported option")
+
+lets_cipher()
